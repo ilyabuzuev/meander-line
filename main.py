17949eb9218c626db9764cd1487c803dbd9daeb8
Ilya Buzuev <ilya.buzuev@mail.ru>
2023-09-28 17:07:49 +0300

    a finite state machine has been added

--- main.py
+++ main.py
@@ -1,166 +1,227 @@
 import matplotlib.pyplot as plt
-import random
 import math
+from tkinter import *
 
-cellSize = 3
-cellsCount = 2
-# cellsMap = [[1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1],
-#             [2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2],
-#             [3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3],
-#             [2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2],
-#             [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]]
+class DielectricFiller:
+  def __init__(self):
+    self.state = 'INIT'
 
-# cellsMap = [[1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1],
-#             [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1],
-#             [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1],
-#             [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1],
-#             [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]]
+    self.xCoords = [0]
+    self.yCoords = [0]
 
-width = cellsCount
-length = cellsCount
+    self.x0 = 0
+    self.y0 = 0
 
-xCoords = [0]
-yCoords = [0]
+    self.defaultCellSize = 5
+    self.defaultWidthX = 11 
+    self.defaultWidthY = 7
 
-x0 = 0
-y0 = 0
+    self.root = Tk()
 
-
-def getCurrentCoords():
-  return x0, y0
-
-def move(direction, value):
-  x, y = getCurrentCoords()
-
-  # print(value)
-
-  global x0
-  global y0
-
-  if (direction == 'up'):
-    x0 = x
-    y0 += value
-
-  if (direction == 'down'):
-    x0 = x
-    y0 -= value
+  def getCurrentState(self):
+    return self.state
   
-  if (direction == 'right'):
-    x0 += value
-    y0 = y
+  def setState(self, state):
+    self.state = state
 
-  if (direction == 'left'):
-    x0 -= value
-    y0 = y
+    self.nextState()
 
-  xCoords.append(x0)
-  yCoords.append(y0)
+  def nextState(self):
+    currentState = self.getCurrentState()
 
-def createCellsMap(widthX, widthY):
-  cellsMap = []
+    if (currentState == 'INIT'):
+      return self.start()
 
-  maxRowNum = math.ceil(widthX / 2)
-  mid = math.ceil(widthY / 2)
+    if (currentState == 'CALCULATE_LINES_TRACE'):
+      return self.calculateLinesTrace()
+    
+    if (currentState == 'PLOT_GRAPHS'):
+      return self.plotGraphs()
+    
+    print(currentState)
 
-  for i in range(mid):
-    row = []
+  def getCurrentCoords(self):
+    return self.x0, self.y0
 
-    for j in range(widthX):
-      if (i < mid):
-        if (j < maxRowNum):
-          row.append(j + 1 + i)
-        else:
-          row.append(widthX - j + i)
+  def move(self, direction, value):
+    x, y = self.getCurrentCoords()
 
-    cellsMap.append(row)
+    if (direction == 'up'):
+      self.x0 = x
+      self.y0 += value
 
-  temp = cellsMap[::-1]
-  
-  if (widthY % 2 != 0):
+    if (direction == 'down'):
+      self.x0 = x
+      self.y0 -= value
+    
+    if (direction == 'right'):
+      self.x0 += value
+      self.y0 = y
+
+    if (direction == 'left'):
+      self.x0 -= value
+      self.y0 = y
+
+    self.xCoords.append(self.x0)
+    self.yCoords.append(self.y0)
+
+  def createCellsMap(self, widthX, widthY):
+    cellsMap = []
+
+    maxRowNum = math.ceil(widthX / 2)
+    mid = math.ceil(widthY / 2)
+
+    for i in range(mid):
+      row = []
+
+      for j in range(widthX):
+        if (i < mid):
+          if (j < maxRowNum):
+            row.append(j + 1 + i)
+          else:
+            row.append(widthX - j + i)
+
+      cellsMap.append(row)
+
+    temp = cellsMap[::-1]
+    
+    if (widthY % 2 != 0):
+      temp.pop(0)
+
+    res = cellsMap + temp
+
+    return res
+
+  def createCell(self, branchesNumber, row, currCell, cellCount, cellSize):
+    lineLength = cellSize
+    offset = cellSize * 1 / (branchesNumber + 2)
+
+    commandParams = ['right', 'down', 'left', 'up']
+    movementCommands = []
+
+    i = 0
+    while (i < branchesNumber + 3):
+      movementCommands.append(commandParams[i % 4])
+      i += 1
+
+    temp = movementCommands[::-1]
     temp.pop(0)
+    res = movementCommands + temp
 
-  res = cellsMap + temp
+    if (row % 2 != 0):
+      for i in range(len(res)):
+        if (res[i] == 'right'):
+          res[i] = 'left'
+          continue
 
-  return res
+        if (res[i] == 'left'):
+          res[i] = 'right'
+          continue
 
-# print( createCellsMap(18, 9) )
+        if (res[i] == 'up'):
+          res[i] = 'down'
+          continue
 
-def createCell(branchesNumber, row, currCell, cellCount, cellSize):
-  lineLength = cellSize
-  offset = cellSize * 1 / (branchesNumber + 2)
+        if (res[i] == 'down'):
+          res[i] = 'up'
+          continue
 
-  commandParams = ['right', 'down', 'left', 'up']
-  movementCommands = []
+    if (row % 2 == 0):
+      self.move('up', lineLength)
 
-  i = 0
-  while (i < branchesNumber + 3):
-    movementCommands.append(commandParams[i % 4])
-    i += 1
-
-  temp = movementCommands[::-1]
-  temp.pop(0)
-  res = movementCommands + temp
-
-  if (row % 2 != 0):
     for i in range(len(res)):
-      if (res[i] == 'right'):
-        res[i] = 'left'
-        continue
-
-      if (res[i] == 'left'):
-        res[i] = 'right'
-        continue
-
-      if (res[i] == 'up'):
-        res[i] = 'down'
-        continue
-
-      if (res[i] == 'down'):
-        res[i] = 'up'
-        continue
-
-  if (row % 2 == 0):
-    move('up', lineLength)
-
-  for i in range(len(res)):
-    if (i <= math.floor(len(res) / 2)):
-      if (i == 0):
-        move(res[i], lineLength)
-      elif (i > 0 and i < math.floor(len(res) / 2) - 1):
-        move(res[i], lineLength - offset)
-        lineLength -= offset
+      if (i <= math.floor(len(res) / 2)):
+        if (i == 0):
+          self.move(res[i], lineLength)
+        elif (i > 0 and i < math.floor(len(res) / 2) - 1):
+          self.move(res[i], lineLength - offset)
+          lineLength -= offset
+        else:
+          self.move(res[i], lineLength - offset)
       else:
-        move(res[i], lineLength - offset)
-    else:
-      if (i == len(res) - 1):
-        move(res[i], lineLength)
-      elif (i > math.floor(len(res) / 2) - 1 and i < len(res) - 2):
-        move(res[i], lineLength - offset)
-        lineLength += offset
-      else:
-        move(res[i], lineLength - offset)
+        if (i == len(res) - 1):
+          self.move(res[i], lineLength)
+        elif (i > math.floor(len(res) / 2) - 1 and i < len(res) - 2):
+          self.move(res[i], lineLength - offset)
+          lineLength += offset
+        else:
+          self.move(res[i], lineLength - offset)
 
-  if (row % 2 != 0 and currCell != cellCount - 1):
-    move('down', lineLength)
+    if (row % 2 != 0 and currCell != cellCount - 1):
+      self.move('down', lineLength)
 
-# def createRandomCellsMap(min, max, length):
-#   for i in range(length):
-#     cellsMap.append(random.randint(min, max))
+  def calculateLinesTrace(self, cellSize=3, widthX=11, widthY=7):
+    self.state = 'CALCULATE_LINES_TRACE'
 
-# createRandomCellsMap(1, 10, cellsCount)
+    cellsMap = self.createCellsMap(widthX, widthY)
 
-cellsMap = createCellsMap(11, 7)
+    for row in range(len(cellsMap)):
+      for cell in range(len(cellsMap[row])):
+        self.createCell(cellsMap[row][cell], row, cell, len(cellsMap[row]), cellSize)
 
-for row in range(len(cellsMap)):
-  for cell in range(len(cellsMap[row])):
-    createCell(cellsMap[row][cell], row, cell, len(cellsMap[row]), cellSize)
+      if (row % 2 == 0):
+        self.move('up', cellSize + cellSize * 1 / 10)
+      elif (row != len(cellsMap) - 1):
+        self.move('up', cellSize * 1 / 10)
 
-  if (row % 2 == 0):
-    move('up', cellSize + cellSize * 1 / 10)
-  elif (row != len(cellsMap) - 1):
-    move('up', cellSize * 1 / 10)
+    print(self.getCurrentState())
 
-# print(xCoords, yCoords)
+    self.setState('PLOT_GRAPHS')
 
-plt.plot(xCoords, yCoords)
-plt.show()
+    return widthX, widthY
+
+  def plotGraphs(self, widthX=11, widthY=7):
+    self.state = 'PLOT_GRAPHS'
+
+    plt.figure(figsize=(widthX, widthY))
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.plot(self.xCoords, self.yCoords)
+    plt.show()
+
+    self.xCoords = [0]
+    self.yCoords = [0]
+
+    self.x0 = 0
+    self.y0 = 0
+
+    print(self.getCurrentState())
+
+    self.setState('INIT')
+
+  def start(self):
+    print(self.getCurrentState())
+
+    self.root.title('Диэлектрический наполнитель')
+    self.root['bg'] = '#deecfa'
+    self.root.geometry('500x200')
+    self.root.resizable(width=False, height=False)
+
+    inputCellSizeLbl = Label(self.root, text='Размер ячейки (мм):', fg='#222', bg='#deecfa', font='Arial 14')
+    inputCellSizeLbl.grid(column=0, row=0, padx=5, pady=5)
+
+    defaultCellSizeValue = IntVar(self.root, value=self.defaultCellSize)
+    inputCellSize = Entry(self.root, bg='white', fg='#222', font='Arial 14', textvariable=defaultCellSizeValue)
+    inputCellSize.grid(column=1, row=0, padx=5, pady=5)
+
+    inputCellsNumberOnX = Label(self.root, text='К-во ячеек по оси X:', fg='#222', bg='#deecfa', font='Arial 14')
+    inputCellsNumberOnX.grid(column=0, row=1, padx=5, pady=5)
+
+    defaultXValue = IntVar(self.root, value=self.defaultWidthX)
+    cellsNumberOnX = Entry(self.root, bg='white', fg='#222', font='Arial 14', textvariable=defaultXValue)
+    cellsNumberOnX.grid(column=1, row=1, padx=5, pady=5)
+
+    inputCellsNumberOnY = Label(self.root, text='К-во ячеек по оси Y:', fg='#222', bg='#deecfa', font='Arial 14')
+    inputCellsNumberOnY.grid(column=0, row=2, padx=5, pady=5)
+
+    defaultYValue = IntVar(self.root, value=self.defaultWidthY)
+    cellsNumberOnY = Entry(self.root, bg='white', fg='#222', font='Arial 14', textvariable=defaultYValue)
+    cellsNumberOnY.grid(column=1, row=2, padx=5, pady=5)
+
+    btn = Button(self.root, text='Рассчитать', bg='#b2d3f3', fg='#222', borderwidth=0, command=lambda: self.calculateLinesTrace(float(inputCellSize.get()), int(cellsNumberOnX.get()), int(cellsNumberOnY.get())), font='Arial 14')
+    btn.grid(row=3, sticky="nsew", padx=5, pady=5)
+
+    self.root.mainloop()
+    
+meander = DielectricFiller()
+meander.setState('INIT')
